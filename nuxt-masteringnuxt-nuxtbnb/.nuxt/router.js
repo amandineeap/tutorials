ae368f14bad69b450c3205a5951b2d9b4bbd60e6
Amandine Eap <amandine@afterdigital.co.uk>
2021-12-21 13:50:52 +0000

    :sparkles: Mastering Nuxt - Pages and Routing

--- nuxt-masteringnuxt-nuxtbnb/.nuxt/router.js
+++ nuxt-masteringnuxt-nuxtbnb/.nuxt/router.js
@@ -4,6 +4,7 @@ import { normalizeURL, decode } from 'ufo'
 import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
+const _2d995f80 = () => interopDefault(import('../pages/home/_id.vue' /* webpackChunkName: "pages/home/_id" */))
 const _4b7889cc = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
 const emptyFn = () => {}
@@ -18,6 +19,10 @@ export const routerOptions = {
   scrollBehavior,
 
   routes: [{
+    path: "/home/:id?",
+    component: _2d995f80,
+    name: "home-id"
+  }, {
     path: "/",
     component: _4b7889cc,
     name: "index"
