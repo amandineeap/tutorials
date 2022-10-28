612668c4be20b4edc9103b337aa45dc195df6ade
Amandine Eap <amandine.eap@gmail.com>
2022-10-28 17:18:31 +0100

    :sparkles: video-23

--- complete-ruby-on-rails-developer-course-udemy/video-19/numbers.rb
+++ complete-ruby-on-rails-developer-course-udemy/video-19/numbers.rb
@@ -1,3 +1,25 @@
+
+
+def multiply(first_num, second_num)
+    first_num.to_f * second_num.to_f
+end
+
+def divide(first_num, second_num)
+    first_num.to_f / second_num.to_f
+end
+
+def substract(first_num, second_num)
+    first_num.to_f - second_num.to_f
+end
+
+def add(first_num, second_num)
+    first_num.to_f + second_num.to_f
+end
+
+def mod(first_num, second_num)
+    first_num.to_f % second_num.to_f
+end
+
 puts "Simple calculator"
 25.times { print "-" }
 
@@ -5,8 +27,8 @@ puts "Enter the first number"
 num_1 = gets.chomp 
 puts "Enter the second number"
 num_2 = gets.chomp
-puts "The first number multiplied by the second number is #{num_1.to_i * num_2.to_i}"
-puts "The first number dividied by the second number is #{num_1.to_i / num_2.to_i}"
-puts "The first number substracted from the second number is #{num_2.to_i - num_1.to_i}"
-puts "The first number added to the second number is #{num_1.to_i + num_2.to_i}"
-puts "The first number mod to the second number is #{num_1.to_i % num_2.to_i}"
+puts "The first number multiplied by the second number is #{multiply(num_1,num_2)}"
+puts "The first number dividied by the second number is #{divide(num_1,num_2)}"
+puts "The first number substracted from the second number is #{substract(num_1,num_2)}"
+puts "The first number added to the second number is #{add(num_1,num_2)}"
+puts "The first number mod to the second number is #{mod(num_1,num_2)}"
