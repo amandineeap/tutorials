ba21aec72c28726c4b5d2c51ffe5a19d2a42fd77
Amandine Eap <amandine.eap@gmail.com>
2022-12-20 16:41:04 +0000

    :sparkles: RoR - create article

--- complete-ruby-on-rails-developer-course-udemy/rails-app/app/controllers/articles_controller.rb
+++ complete-ruby-on-rails-developer-course-udemy/rails-app/app/controllers/articles_controller.rb
@@ -6,4 +6,20 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+  end
+
+  def create
+    # render plain: params[:article] # need data: { turbo: false } on form_with to work
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    # render plain: @article.inspect # need data: { turbo: false } on form_with to work
+    if @article.save
+      flash[:notice] = "Your Article was Created"
+      redirect_to article_path(@article)
+    else
+      flash[:notice] = @article.errors.full_messages
+      render 'new', status: 422
+    end
+  end
 end
