b89e87df6c5f689ec12834055c0ec96a9d86eace
Amandine Eap <amandine.eap@gmail.com>
2022-12-23 11:26:49 +0000

    :sparkles: RoR - update users table

--- complete-ruby-on-rails-developer-course-udemy/rails-app/db/schema.rb
+++ complete-ruby-on-rails-developer-course-udemy/rails-app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_23_111052) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_23_112235) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_23_111052) do
     t.string "username"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
