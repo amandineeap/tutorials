0df590706181fd30d6ac1020f28a77d332fe6aba
Amandine Eap <amandine.eap@gmail.com>
2022-12-26 15:52:42 +0000

    :sparkles: RoR - Admin

--- complete-ruby-on-rails-developer-course-udemy/rails-app/app/controllers/application_controller.rb
+++ complete-ruby-on-rails-developer-course-udemy/rails-app/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user, :logged_in?
+  helper_method :current_user, :logged_in?, :is_admin?
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
     # memoization - only fetch from db if not already set
@@ -14,5 +15,9 @@ class ApplicationController < ActionController::Base
       flash[:alert] = "You must be logged in to perform that action"
       redirect_to login_path
     end
+  end
+
+  def is_admin?
+    logged_in? && current_user.admin == true
   end
 end
