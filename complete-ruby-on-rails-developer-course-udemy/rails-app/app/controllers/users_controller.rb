c420935f971b9d08026931ed738928f3a0d6ac90
Amandine Eap <amandine.eap@gmail.com>
2022-12-26 14:17:35 +0000

    :sparkles: RoR - cleanup url redirects

--- complete-ruby-on-rails-developer-course-udemy/rails-app/app/controllers/users_controller.rb
+++ complete-ruby-on-rails-developer-course-udemy/rails-app/app/controllers/users_controller.rb
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:notice] = "User was created successfully"
-      redirect_to articles_path
+      redirect_to users_path
     else
       render 'new', status: 422
     end
@@ -34,7 +34,7 @@ class UsersController < ApplicationController
   def update
     if @user.update(user_params)
       flash[:notice] = "User was updated successfully"
-      redirect_to articles_path
+      redirect_to users_path
     else 
       render 'edit'
     end
@@ -43,11 +43,7 @@ class UsersController < ApplicationController
   # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
-
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to users_path
   end
 
   private
