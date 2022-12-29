fb96b6f3eef074966ce3b92235564327145776e2
Amandine Eap <amandine.eap@gmail.com>
2022-12-29 09:41:59 +0000

    :sparkles: message_me - add jquery and nav partial

--- complete-ruby-on-rails-developer-course-udemy/message_me/app/javascript/application.js
+++ complete-ruby-on-rails-developer-course-udemy/message_me/app/javascript/application.js
@@ -1,5 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
-//= jquery
+import "jquery"
+import "jquery_ujs"
+
+//= require jquery
+//= require jquery_ujs
+//= require turbolinks
 //= require semantic-ui
+//= require_tree .
+
+$(function() {     
+  $( document ).ready(function() {
+    console.log('hello')
+    // $(".ui.dropdown").dropdown();   
+  });
+}); 
+
