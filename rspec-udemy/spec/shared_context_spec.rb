1cc03baf9206b26156e970d18fd940ef4f51ee65
Amandine Eap <amandine.eap@gmail.com>
2023-03-16 15:08:37 +0000

    :sparkles: Update shared context spec

--- rspec-udemy/spec/shared_context_spec.rb
+++ rspec-udemy/spec/shared_context_spec.rb
@@ -27,3 +27,11 @@ RSpec.describe 'first example group' do
     expect(some_helper_method).to eq(5)
   end
 end
+
+RSpec.describe 'second example in different file' do
+  include_context 'common'
+
+  it 'can use shared let variables' do
+    expect(some_variable).to eq([1, 2, 3])
+  end
+end
