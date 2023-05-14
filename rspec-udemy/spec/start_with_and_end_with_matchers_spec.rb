f5333000ce561390b7ca1d450afa442a391f788f
Amandine Eap <amandine.eap@gmail.com>
2023-05-14 11:20:19 +0100

    :sparkles: Extra on start with end with spec

---
+++ rspec-udemy/spec/start_with_and_end_with_matchers_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe 'start_with and end_with matchers' do
+  describe 'caterpillar' do
+    it 'should check for substring at the begining or end' do 
+      expect(subject).to start_with('cat')
+      expect(subject).to end_with('pillar')
+    end
+  end
+
+  describe [:a, :b, :c, :d] do
+    it 'should check for elements at beginning or end of the array' do
+      expect(subject).to start_with(:a)
+      expect(subject).to start_with(:a, :b)
+      expect(subject).to start_with(:a, :b, :c)
+      expect(subject).to end_with(:d)
+      expect(subject).to end_with(:c, :d)
+    end
+
+    it { is_expected.to start_with(:a, :b) }
+  end
+end
